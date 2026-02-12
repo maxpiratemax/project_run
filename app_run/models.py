eb7658f40520cd967f6b2122d93a759e2de043e7
Max <max_pirate@mail.ru>
2026-02-12 21:49:19 +0300

    Create model Run, create serializers.py, add ViewSet and router

--- app_run/models.py
+++ app_run/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Run(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(verbose_name="Комментарий")
+    athlete = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Атлет")
+
+    class Meta:
+        verbose_name = "Забег"
+        verbose_name_plural = "Забеги"
+
+    def __str__(self):
+        return f"{self.athlete} — {self.comment[:40]}"
--- app_run/views.py
+++ app_run/views.py
@@ -1,7 +1,11 @@
 from django.conf import settings
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from app_run.models import Run
+from app_run.serializers import RunSerializer
 
 
 # Create your views here.
@@ -13,3 +17,7 @@ def company_details(request):
         'contacts': settings.CONTACTS,
     }
     return Response(details)
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
