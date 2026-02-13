793260fc91d72e02a8df25fc368a2a3082d5b24b
Max <max_pirate@mail.ru>
2026-02-13 23:06:22 +0300

    5. “Find user”

--- app_run/views.py
+++ app_run/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 
 from app_run.models import Run
@@ -26,6 +27,8 @@ class RunViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['first_name', 'last_name']
 
     def get_queryset(self):
         qs = self.queryset.filter(is_superuser=False)
